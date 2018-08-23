2f540d9e1304f185850f72ca096e07f1c402f09c
Martin Stiaszny <stiaszny@gmail.com>
2018-08-23 16:20:59 -0700

    Remove personal developer information.

--- include/swiftnav/ephemeris.h
+++ include/swiftnav/ephemeris.h
@@ -1,7 +1,6 @@
 /*
  * Copyright (C) 2010, 2016 Swift Navigation Inc.
  * Contact: Swift Navigation <dev@swiftnav.com>
- *          Fergus Noble <fergus@swift-nav.com>
  *
  * This source is subject to the license found in the file 'LICENSE' which must
  * be distributed together with this source. All other rights reserved.
