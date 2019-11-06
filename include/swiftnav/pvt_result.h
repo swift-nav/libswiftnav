06b6de54476e593d1fe72e8c32a3899021d790d2
Guillaume Decerprit <guillaume@swift-nav.com>
2019-11-05 16:48:56 -0800

    Add bit masks for INS_STATUS sbp msg

--- include/swiftnav/pvt_result.h
+++ include/swiftnav/pvt_result.h
@@ -49,6 +49,8 @@ extern "C" {
 #define INS_STATUS_ALIGNING 1
 #define INS_STATUS_READY 2
 #define INS_STATUS_OUTAGE_EXCEEDS_MAX 3
+#define INS_MODE_MASK 0x7  /* Bit 0-2 */
+#define INS_GNSS_FIX_AVAILABILITY_MASK 0x8  /* Bit 3 */
 
 /* These masks are used in the firmware. */
 #define POSITION_MODE_MASK 0x07 /* Bits 0-2 */
